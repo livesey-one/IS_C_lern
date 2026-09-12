709d42f4826d7f8d9be1182915292d3fb1e8af54
Andrey <andreymarche@icloud.com>
2026-09-12 16:53:10 +0700

    first

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  // int b;
+  // scanf("%d", &b);
+  // for (short a = 0; a < b; a++) {
+  //   printf("%d\n", a)
+  // }
+  int a = 1;
+  while (a > 0) {
+    scanf("%d", &a);
+  };
+  return 0;
+}
