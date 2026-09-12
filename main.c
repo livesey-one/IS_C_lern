07e4e43c6b50603166ef7ccb1c1170d9b9864a08
Andrey <andreymarche@icloud.com>
2026-09-12 17:06:04 +0700

    PRAKRICA_1

--- main.c
+++ main.c
@@ -7,9 +7,22 @@ int main() {
   // for (short a = 0; a < b; a++) {
   //   printf("%d\n", a)
   // }
-  int a = 1;
-  while (a > 0) {
-    scanf("%d", &a);
-  };
+  // int a = 1;
+  // while (a > 0) {
+  //   scanf("%d", &a);
+  // };
+
+  int chet = 0;
+  int nechet = 0;
+
+  for (short i = 0; i < 21; i++) {
+    if (i % 2 == 0) {
+      chet++;
+    } else {
+      nechet++;
+    }
+  }
+  printf(" %d, %d\n", chet, nechet);
+
   return 0;
 }
